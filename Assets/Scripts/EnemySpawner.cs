1291aeab14d021e09b01a4038c29d6af39ec3f7e
Vinicius Guimarães <vinicius.guimaraes5@fatec.sp.gov.br>
2022-04-07 19:54:31 -0300

    add colliders, tags and take damage functions

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -10,35 +10,31 @@ public class EnemySpawner : MonoBehaviour
     public float spawnDelay = 1.0f;
     private float now = 0.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void FixedUpdate()
-    { 
-        if(now >= spawnDelay)
+    {
+        if (now >= spawnDelay)
         {
             SortPosition();
-        } else
+        }
+        else
         {
             now += Time.fixedDeltaTime;
         }
     }
 
-    private void SortPosition ()
+    private void SortPosition()
     {
         int enemy = Random.Range(0, enemyOptions.Length);
         float posX = Random.Range(-radio, radio);
         float posY = Mathf.Sqrt(Mathf.Pow(radio, 2) - Mathf.Pow(posX, 2));
+
         if (Random.value > 0.5f)
         {
             posY *= -1.0f;
         }
 
-        Instantiate(enemyOptions[enemy], new Vector3(posX, posY, 0.0f) , Quaternion.identity);
+        Instantiate(enemyOptions[enemy], new Vector3(posX, posY, 0.0f), Quaternion.identity);
 
         now = 0.0f;
     }
